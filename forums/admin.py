c06ced8cf8ff596ddcda8c4882a2c43792f27d51
Khalifa1997 <ahmed70000@gmail.com>
2018-06-24 01:14:15 +0200

    Admin- forums

--- forums/admin.py
+++ forums/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import thread,reply,board
 
 # Register your models here.
+admin.site.register(thread)
+admin.site.register(reply)
+admin.site.register(board)
